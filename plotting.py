f4d038b62349bf8a1dcb182c8e3aa73c679fff91
Sina <stavakkoli1999@gmail.com>
2025-07-16 15:51:16 +0330

    Fix: plotting.py debugged

--- plotting.py
+++ plotting.py
@@ -1,14 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from options import Option
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 for 3D projection
 
+def plot_greeks_vs_time(S, K, r, sigma, option_type='call', T_max=1, steps=100):
+    times = np.linspace(T_max, 0.001, steps)
+    deltas, gammas, thetas, vegas, rhos = [], [], [], [], []
+
+    for T in times:
+        opt = Option(S, K, T, r, sigma, option_type)
+        greeks = opt.greeks()
+        deltas.append(greeks['Delta'])
+        gammas.append(greeks['Gamma'])
+        thetas.append(greeks['Theta'])
+        vegas.append(greeks['Vega'])
+        rhos.append(greeks['Rho'])
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(times, deltas, label='Delta')
+    plt.plot(times, gammas, label='Gamma')
+    plt.plot(times, thetas, label='Theta')
+    plt.plot(times, vegas, label='Vega')
+    plt.plot(times, rhos, label='Rho')
+    plt.gca().invert_xaxis()
+    plt.title(f'{option_type.capitalize()} Option Greeks Evolution vs Time')
+    plt.xlabel('Time to Maturity (Years)')
+    plt.ylabel('Greek Value')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 def plot_delta_gamma_surface(K, r, sigma, option_type='call', S_range=(50, 150), T_range=(0.01, 1), steps=50):
-    """
-    Plot 3D surface of Delta and Gamma vs Stock Price and Time to Maturity.
-    """
     S_vals = np.linspace(S_range[0], S_range[1], steps)
     T_vals = np.linspace(T_range[0], T_range[1], steps)
 
@@ -42,44 +65,4 @@ def plot_delta_gamma_surface(K, r, sigma, option_type='call', S_range=(50, 150),
     fig.colorbar(surf2, ax=ax2, shrink=0.5, aspect=10)
 
     plt.suptitle(f'{option_type.capitalize()} Option Greeks Surfaces')
-    plt.show()
-
-
-def plot_greeks_vs_time(S, K, r, sigma, option_type='call', T_max=1, steps=100):
-    """
-    Plot Greeks evolution as time to maturity approaches zero.
-    
-    Parameters:
-        S: Stock price
-        K: Strike price
-        r: Risk-free rate
-        sigma: Volatility
-        option_type: 'call' or 'put'
-        T_max: Initial time to maturity (years)
-        steps: Number of time steps until expiration
-    """
-    times = np.linspace(T_max, 0.001, steps)  # Avoid zero division
-    deltas, gammas, thetas, vegas, rhos = [], [], [], [], []
-
-    for T in times:
-        opt = Option(S, K, T, r, sigma, option_type)
-        greeks = opt.greeks()
-        deltas.append(greeks['Delta'])
-        gammas.append(greeks['Gamma'])
-        thetas.append(greeks['Theta'])
-        vegas.append(greeks['Vega'])
-        rhos.append(greeks['Rho'])
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(times, deltas, label='Delta')
-    plt.plot(times, gammas, label='Gamma')
-    plt.plot(times, thetas, label='Theta')
-    plt.plot(times, vegas, label='Vega')
-    plt.plot(times, rhos, label='Rho')
-    plt.gca().invert_xaxis()  # Show time approaching zero from left to right
-    plt.title(f'{option_type.capitalize()} Option Greeks Evolution vs Time to Maturity')
-    plt.xlabel('Time to Maturity (Years)')
-    plt.ylabel('Greek Value')
-    plt.legend()
-    plt.grid(True)
     plt.show()
